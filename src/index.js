059db446d6496722725571c61b65ee8c0b7db738
Dharmesh <dharmeshkota123@gmail.com>
2024-12-16 14:07:28 +0530

    Removed react-redux and shifted to Context-API

--- src/index.js
+++ src/index.js
@@ -1,26 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 
 import "./index.css";
 import App from "./App";
 
-import { store, persistor } from "./redux/store";
 import CartProvider from "./providers/cart/cart.provider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <CartProvider>
-    <Provider store={store}>
       <React.StrictMode>
         <BrowserRouter>
-          <PersistGate persistor={persistor}>
             <App />
-          </PersistGate>
         </BrowserRouter>
       </React.StrictMode>
-    </Provider>
   </CartProvider>
 );
